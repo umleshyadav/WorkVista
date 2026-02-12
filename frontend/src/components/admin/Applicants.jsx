cd23efa3627da6d53c0fed7cac4184848973286a
Umlesh Yadav <167521495+umleshyadav@users.noreply.github.com>
2026-02-12 18:20:45 +0530

    Update Applicants.jsx

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Navbar from '../shared/Navbar'
 import ApplicantsTable from './ApplicantsTable'
 import axios from 'axios';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAllApplicants } from '@/redux/applicationSlice';
