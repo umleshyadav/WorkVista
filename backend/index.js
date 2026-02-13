157522bb77ae645ea5cc4d787a6f54f2580f42df
Umlesh Yadav <umleshyadav9506@gmail.com>
2026-02-13 17:49:29 +0530

    for production

--- backend/index.js
+++ backend/index.js
@@ -11,22 +11,23 @@ import applicationRoute from "./routes/application.route.js";
 import path from "path";
 
 dotenv.config({});
-
+//connect db
+connectDB();
+const PORT=process.env.PORT || 8000;
 const app=express();
 
+const _dirname=path.resolve();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-      origin: "http://localhost:5173",
+      origin:"http://localhost:5173",
       credentials:true
 };
-
 app.use(cors(corsOptions));
-
-const PORT=process.env.PORT || 8000;
 
 //api's route
 app.use("/api/v1/user",userRoute);
@@ -34,8 +35,13 @@ app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 
 app.listen(PORT,()=>{
-    connectDB();
     console.log(`server running at port ${PORT}`);
 });
+
+
